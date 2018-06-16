ea9ee5d7023ebe42c5bcc3daefd1e8428fb66185
Oleg Panagushin <opanagushin@gmail.com>
2018-06-16 14:53:54 +0300

    better chart appearance

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import {
   LineChart,
   Line,
@@ -12,6 +13,7 @@ import {
   Bar
 } from "recharts";
 import moment from "moment";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 const dateFormat = v =>
   moment
@@ -19,34 +21,67 @@ const dateFormat = v =>
     .local()
     .format("H:mm");
 
+const styles = {
+  chart: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center"
+  },
+  loader: {
+    padding: 4
+  }
+};
+
 export const LikesChart = connect(state => ({
-  data: state.charts.likes
-}))(({ data }) => {
-  return (
-    <ResponsiveContainer width="100%" height={150}>
-      <LineChart data={data}>
-        <Line type="monotone" dataKey="likes" stroke="#3f51b5" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="when" tickFormatter={dateFormat} scale="utcTime" />
-        <YAxis tickCount={3} />
-        <Tooltip labelFormatter={dateFormat} />
-      </LineChart>
-    </ResponsiveContainer>
-  );
-});
+  data: state.charts.likes,
+  wait: state.root.fetchingDashboard || state.root.updateDashboard
+}))(
+  withStyles(styles)(({ data, wait, classes }) => {
+    return (
+      <ResponsiveContainer width="100%" height={150} className={classes.chart}>
+        {wait ? (
+          <CircularProgress size={40} className={classes.loader} />
+        ) : (
+          <LineChart data={data}>
+            <Line type="monotone" dataKey="likes" stroke="#3f51b5" />
+            <CartesianGrid
+              stroke="#ccc"
+              strokeDasharray="3 3"
+              vertical={false}
+            />
+            <XAxis dataKey="when" tickFormatter={dateFormat} scale="utcTime" />
+            <YAxis tickCount={3} />
+            <Tooltip labelFormatter={dateFormat} />
+          </LineChart>
+        )}
+      </ResponsiveContainer>
+    );
+  })
+);
 
 export const CommentsChart = connect(state => ({
-  data: state.charts.comments
-}))(({ data }) => {
-  return (
-    <ResponsiveContainer width="100%" height={150}>
-      <BarChart data={data}>
-        <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="when" tickFormatter={dateFormat} />
-        <YAxis tickCount={3} />
-        <Tooltip labelFormatter={dateFormat} />
-        <Bar dataKey="comments" fill="#3f51b5" />
-      </BarChart>
-    </ResponsiveContainer>
-  );
-});
+  data: state.charts.comments,
+  wait: state.root.fetchingDashboard || state.root.updateDashboard
+}))(
+  withStyles(styles)(({ data, wait, classes }) => {
+    return (
+      <ResponsiveContainer width="100%" height={150} className={classes.chart}>
+        {wait ? (
+          <CircularProgress size={40} className={classes.loader} />
+        ) : (
+          <BarChart data={data}>
+            <CartesianGrid
+              stroke="#ccc"
+              strokeDasharray="3 3"
+              vertical={false}
+            />
+            <XAxis dataKey="when" tickFormatter={dateFormat} />
+            <YAxis tickCount={3} />
+            <Tooltip labelFormatter={dateFormat} />
+            <Bar dataKey="comments" fill="#3f51b5" />
+          </BarChart>
+        )}
+      </ResponsiveContainer>
+    );
+  })
+);
